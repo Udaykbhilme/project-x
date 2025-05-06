9f4f9ef4466eb3d2df58979c863583d496751b74
Uday K Bhilme <144208157+Udaykbhilme@users.noreply.github.com>
2025-05-06 12:30:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+// No more lovable-tagger here
+export default defineConfig({
+  base: "project-x", // This is IMPORTANT for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
